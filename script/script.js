8abfb4e5a2128dc9e0feaab9b8734e36f423d123
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-07-25 10:29:49 +0200

    updates

--- script/script.js
+++ script/script.js
@@ -3,25 +3,6 @@
 var title = document.getElementById('titleFendi')
 var subtitle = document.getElementById('subtitleFendi')
 
-TweenMax.from(title, .8, {x: 500, delay:.3, ease: Power4.easeOut})
-TweenMax.from(subtitle, 1, {x: -500, delay:.3, ease: Power4.easeOut})
-TweenMax.from('#fendiMainImg', 1, {y: 700, delay:.6, ease: Power4.easeOut})
-
-var myAnimation = new TimelineMax();
-var myAnimation2 = new TimelineMax();
-
-myAnimation.from("#beginning-h1", 0.5, {y: -500});
-myAnimation2.from("#beginning-img", 0.5, {x: 400});
-
-var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#beginnerTrigger",
-  triggerHook: .1,
-  offset: 0,
-  duration: 300,
-  opacity:1
-}).setTween(myAnimation).addTo(controller)
-
 //Portfolio Animation
 TweenMax.to('.header-wrapper h1', 1,{x: 0, opacity: 1})
 
@@ -31,3 +12,7 @@ function workInProgress() {
   alert('Work in progres page')
 }
 
+//Fendi animations
+TweenMax.from(title, .8, {x: 500, delay:.3, ease: Power4.easeOut})
+TweenMax.from(subtitle, 1, {x: -500, delay:.3, ease: Power4.easeOut})
+TweenMax.from('#fendiMainImg', 1, {y: 700, delay:.6, ease: Power4.easeOut})
